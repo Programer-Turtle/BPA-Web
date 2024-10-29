13d26cacfabb096d697e43c6fbc7e5786979c887
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2024-10-29 15:57:41 -0400

    Add files via upload

---
+++ scripts/TourDates.js
@@ -0,0 +1,27 @@
+let Tours = [
+    {
+        'Name':"On Strike",
+        'Dates':"November 22 - December 17, 2024",
+        'Location':"Europe",
+        'Cities':[]
+    },
+    {
+        'Name':"All Around",
+        'Dates':"March 13-30, 2025",
+        'Location':"United States",
+        "Cities":[
+            'Washigton DC, March 13',
+            'Charleston, WV, March 16',
+            'Indianapolis, IN, March 19',
+            'Chicago, IL, March 22',
+            'Houston, TX, March 26',
+            'Los Angeles, CA, March 30'
+        ]
+    },
+    {
+        'Name':"Out of Pills",
+        'Dates':"October 5 - November 2, 2025",
+        'Location':"Canada",
+        "Cities":[]
+    }
+]
