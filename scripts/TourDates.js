593d725920ee4f411175f96d31904779dd27db8f
Informare <karsonulerick@gmail.com>
2024-11-29 15:24:47 -0500

    Added Contacts

--- scripts/TourDates.js
+++ scripts/TourDates.js
@@ -31,9 +31,23 @@ let HoldBox = document.getElementById("HoldBox")
 for(let TourIndex=0; TourIndex<Tours.length; TourIndex++){
     let TourBox = document.createElement("div")
     let CurrentTourName = Tours[TourIndex]["Name"]
+    let CurrentTourDates = Tours[TourIndex]["Dates"]
+    let CurrentTourLocation = Tours[TourIndex]["Location"]
+    let CurrentTourCities = Tours[TourIndex]["Cities"]
     TourBox.className = "TourBox"
     TourBox.id = "TourBox"
-    TourBox.innerHTML = `<div id="TourBox" class="TourBox"><h1>${CurrentTourName}</h1></div>`
+    TourBox.innerHTML = `<div id="TourBox" class="TourBox">
+    <h1>${CurrentTourName}</h1>
+    <h2 style="margin-top: 30px;" >Dates</h2>
+    <h3>${CurrentTourDates}</h3>
+    <h2 style="margin-top: 30px;" >Location</h2>
+    <h3>${CurrentTourLocation}</h3>
+    <h2 style="margin-top: 30px;" >Cities</h2>`
+
+    for(let CitiesIndex=0; CitiesIndex<CurrentTourCities.length; CitiesIndex++){
+        TourBox.innerHTML += `<h3>${CurrentTourCities[CitiesIndex]}</h3>`
+    }
+    TourBox.innerHTML+="</div>"
     HoldBox.appendChild(TourBox)
 }
 
